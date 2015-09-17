9f261096b20f571b07da87847a93adb73d371b44
Behnam Ghafary <behnam.ghafary@gmail.com>
2015-09-17 21:41:09 +0430

    no message

--- _/components/js/script.js
+++ _/components/js/script.js
@@ -1,10 +1,9 @@
 
 $(document).ready(function() {
     $(".mCustomScrollbar").mCustomScrollbar({axis:"x"});
-
-    function redirect() {
-    document.location.href = '#/browser/' + document.getElementById('search').value;
-    document.getElementById('search').value = "";
-    return false;
-    };
 });
+function redirect() {
+document.location.href = '#/browser/' + document.getElementById('search').value;
+document.getElementById('search').value = "";
+return false;
+};
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var morgan = require("morgan");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 
-//-----------------------------
+//----------------------------
 //mongoose.connect("mongodb://ayandeyeman:locked@ds047792.mongolab.com:47792/ayandeyeman"); // connect to our database
 mongoose.connect("mongodb://behnam:locked@ds041623.mongolab.com:41623/medicaldevice"); // connect to our database
 
